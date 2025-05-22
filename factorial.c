39e7301231afe54dd443eebc6ca8759b3fddcb1c
Rachkid <abouzeidirachid97@gmail.com>
2025-05-22 03:28:25 +0100

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+main()
+{
+  // ce code calcul la factorielle d'un nombre entrer par l'utilisateur 
+  int n,i,f
+  printf("Donnez un nombre: \n");
+  scanf("%d",&n);
+  f=1;
+  for(i=1; i<=n; i++)
+    {
+      f=f*i;
+    }
+  printf("la factorielle de %d est: %d \n", n,f);
+  return 0;
+}
