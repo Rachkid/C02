c4fcd094cbe3675f95925dc61b35ea81ee7cbfc5
Rachkid <abouzeidirachid97@gmail.com>
2025-05-22 03:39:20 +0100

    Create sum_two_integers.c

---
+++ sum_two_integers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+  int a,b,sum;
+  printf("Entrez le premier nombre:\n");
+  scanf("%d%",&a);
+  printf("Entrez le deuxième nombre:\n");
+  scanf("%d",&b);
+  sum = a + b;
+  printf("la somme est:%d\n", sum);
+  return 0;
+}
